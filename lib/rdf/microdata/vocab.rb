65c0c7e91bf613b5a9362f1e8505aef30c13db51
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-11-18 16:58:17 -0800

    Updated microdata namespace.

--- lib/rdf/microdata/vocab.rb
+++ lib/rdf/microdata/vocab.rb
@@ -1,5 +1,5 @@
 module RDF
-  class MD < Vocabulary("http://www.w3.org/1999/xhtml/microdata#"); end
+  class MD < Vocabulary("http://www.w3.org/ns/md#"); end
   class Schema < Vocabulary("http://schema.org/"); end
   class XHV < Vocabulary("http://www.w3.org/1999/xhtml/vocab#"); end
 end
