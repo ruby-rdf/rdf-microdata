b735f96b95c533da2fa796616bfb636b113b98e7
Gregg Kellogg <gregg@kellogg-assoc.com>
2012-03-01 10:56:11 -0800

    Add hCard and hCalendar vocabulary definitions.

--- lib/rdf/microdata/vocab.rb
+++ lib/rdf/microdata/vocab.rb
@@ -2,4 +2,6 @@ module RDF
   class MD < Vocabulary("http://www.w3.org/ns/md#"); end
   class Schema < Vocabulary("http://schema.org/"); end
   class XHV < Vocabulary("http://www.w3.org/1999/xhtml/vocab#"); end
+  class HCard < Vocabulary("http://microformats.org/profile/hcard#"); end
+  class HCalendar < Vocabulary("http://microformats.org/profile/hcalendar"); end
 end
