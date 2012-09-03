8757e7e2d651b745fc28b3652efd2eb04a1493f5
Gregg Kellogg <gregg@kellogg-assoc.com>
2012-09-03 12:11:07 -0600

    Update suite spec based on new format of tests and manifests. Use the JSON-LD version of the manifest as the driver.

--- spec/suite_helper.rb
+++ spec/suite_helper.rb
@@ -4,85 +4,154 @@ require 'spira'
 require 'rdf/turtle'
 require 'open-uri'
 
+# For now, override RDF::Utils::File.open_file to look for the file locally before attempting to retrieve it
+module RDF::Util
+  module File
+    REMOTE_PATH = "http://www.w3.org/TR/microdata-rdf/tests/"
+    LOCAL_PATH = ::File.expand_path("../htmldata/microdata-rdf/tests", __FILE__) + '/'
+
+    ##
+    # Override to use Patron for http and https, Kernel.open otherwise.
+    #
+    # @param [String] filename_or_url to open
+    # @param  [Hash{Symbol => Object}] options
+    # @option options [Array, String] :headers
+    #   HTTP Request headers.
+    # @return [IO] File stream
+    # @yield [IO] File stream
+    def self.open_file(filename_or_url, options = {}, &block)
+      #puts "open #{filename_or_url}"
+      case filename_or_url.to_s
+      when /^file:/
+        path = filename_or_url[5..-1]
+        Kernel.open(path.to_s, &block)
+      when /^#{REMOTE_PATH}/
+        begin
+          #puts "attempt to open #{filename_or_url} locally"
+          #puts " => #{filename_or_url.to_s.sub(REMOTE_PATH, LOCAL_PATH)}"
+          response = ::File.open(filename_or_url.to_s.sub(REMOTE_PATH, LOCAL_PATH))
+          case filename_or_url.to_s
+          when /\.html$/
+            def response.content_type; 'text/html'; end
+          when /\.ttl$/
+            def response.content_type; 'text/turtle'; end
+          when /\.json$/
+            def response.content_type; 'application/json'; end
+          when /\.jsonld$/
+            def response.content_type; 'application/ld+json'; end
+          else
+            def response.content_type; 'unknown'; end
+          end
+          #puts "use #{filename_or_url} locally as #{response.content_type}"
+
+          if block_given?
+            begin
+              yield response
+            ensure
+              response.close
+            end
+          else
+            response
+          end
+        rescue Errno::ENOENT
+          # Not there, don't run tests
+          Kernel.open(path.to_s, &block)
+        end
+      else
+      end
+    end
+  end
+end
+
+module JSON::LD
+  # Simple Ruby reflector class to provide native
+  # access to JSON-LD objects
+  class Resource
+    # Object representation of resource
+    # @attr [Hash<String => Object] attributes
+    attr :attributes
+
+    # ID of this resource
+    # @attr [String] id
+    attr :id
+    
+    # A new resource from the parsed graph
+    # @param [Hash{String => Object}] node_definition
+    def initialize(node_definition)
+      @attributes = node_definition
+      @attributes.delete('@context') # Don't store with object
+      @id = @attributes['@id']
+      @anon = @id.nil? || @id.to_s[0,2] == '_:'
+    end
+
+    # Values of all properties other than id and type
+    def property_values
+      attributes.dup.delete_if {|k, v| %(id type).include?(k)}.values
+    end
+
+    # Access individual fields, from subject definition
+    def property(prop_name); @attributes.fetch(prop_name, nil); end
+
+    # Access individual fields, from subject definition
+    def method_missing(method, *args)
+      property(method.to_s)
+    end
+
+    def inspect
+      "<Resource" +
+      attributes.dup.keep_if {|k, v| %(@id @type comment).include?(k)}.map do |k, v|
+        "\n  #{k}: #{v.inspect}"
+      end.join(" ") +
+      ">"
+    end
+  end
+end
+
 module Fixtures
   module SuiteTest
-    class MF < RDF::Vocabulary("http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#"); end
-    SUITE_BASE = File.expand_path(File.dirname(__FILE__))  + "/test-suite/"
+    class Manifest < JSON::LD::Resource
+      def self.open(file)
+        #puts "open: #{file}"
+        RDF::Util::File.open_file(file) do |f|
+          json = JSON.parse(f.read)
+          self.from_jsonld(json)
+        end
+      end
 
-    class Manifest < Spira::Base
-      type MF.Manifest
-      property :entry_list, :predicate => MF['entries']
-      property :comment,    :predicate => RDFS.comment
+      # @param [Hash] json framed JSON-LD
+      # @return [Array<Manifest>]
+      def self.from_jsonld(json)
+        json['@graph'].map {|e| Manifest.new(e)}
+      end
+
+      def entries
+        # Map entries to resources
+        attributes['entries'].map {|e| Entry.new(e)}
+      end
     end
 
-    class Entry
+    class Entry < JSON::LD::Resource
       attr_accessor :debug
-      attr_accessor :compare
-      include Spira::Resource
-      type MF["Entry"]
 
-      property :name,     :predicate => MF["name"],         :type => XSD.string
-      property :comment,  :predicate => RDF::RDFS.comment,  :type => XSD.string
-      property :result,   :predicate => MF.result
-      has_many :action,   :predicate => MF["action"]
-
-      def input
-        Kernel.open(self.inputDocument.to_s)
+      # Alias data and query
+      def data
+        self.action['data']
       end
       
-      def output
-        self.result ? Kernel.open(self.result.to_s) : ""
-      end
-
-      def inputDocument
-        self.class.repository.first_object(:subject => self.action.first)
-      end
-
-      def base_uri
-        inputDocument.to_s
+      def query
+        self.action['query']
       end
       
-      def inspect
-        "[#{self.class.to_s} " + %w(
-          subject
-          name
-          comment
-          result
-          inputDocument
-        ).map {|a| v = self.send(a); "#{a}='#{v}'" if v}.compact.join(", ") +
-        "]"
+      def registry
+        self.action.fetch('registry',
+          "http://www.w3.org/TR/microdata-rdf/tests/test-registry.json")
       end
-    end
 
-    class Good < Manifest
-      default_source :turtle
-
-      def entries
-        RDF::List.new(entry_list, self.class.repository).map { |entry| entry.as(GoodEntry) }
+      def result
+        property('result') == 'true'
       end
+      
+      def trace; @debug.join("\n"); end
     end
-    
-    class Bad < Manifest
-      default_source :turtle_bad
-
-      def entries
-        RDF::List.new(entry_list, self.class.repository).map { |entry| entry.as(BadEntry) }
-      end
-    end
-
-    class GoodEntry < Entry
-      default_source :turtle
-    end
-
-    class BadEntry < Entry
-      default_source :turtle_bad
-    end
-
-    # Note that the texts README says to use a different base URI
-    tests = RDF::Repository.load(SUITE_BASE + "manifest.ttl")
-    Spira.add_repository! :turtle, tests
-    
-    tests_bad = RDF::Repository.load(SUITE_BASE + "manifest-bad.ttl")
-    Spira.add_repository! :turtle_bad, tests_bad
   end
 end
--- spec/suite_spec.rb
+++ spec/suite_spec.rb
@@ -3,55 +3,37 @@ require 'spec_helper'
 
 describe RDF::Microdata::Reader do
   # W3C Microdata Test suite from FIXME
-  describe "w3c microdata tests" do
+  describe "w3c microdata tests", :ci => ENV['CI'] do
     require 'suite_helper'
+    MANIFEST = "http://www.w3.org/TR/microdata-rdf/tests/manifest.jsonld"
 
-    describe "positive parser tests" do
-      Fixtures::SuiteTest::Good.each do |m|
+    Fixtures::SuiteTest::Manifest.open(MANIFEST).each do |m|
+      describe m.comment do
         m.entries.each do |t|
-          #puts t.inspect
           specify "#{t.name}: #{t.comment}" do
             t.debug = []
-            reader = RDF::Microdata::Reader.new(t.input,
-              :base_uri => t.inputDocument,
+            t.debug << "base: #{t.data}"
+            reader = RDF::Microdata::Reader.open(t.data,
+              :base_uri => t.data,
               :strict => true,
               :validate => true,
+              :registry_uri => t.registry,
               :debug => t.debug)
             reader.should be_a RDF::Reader
 
             graph = RDF::Graph.new << reader
 
-            #puts "parse #{self.outputDocument} as #{RDF::Reader.for(self.outputDocument)}"
-            output_graph = RDF::Graph.load(t.result, :base_uri => t.inputDocument)
-            puts "result: #{CGI.escapeHTML(graph.to_ntriples)}" if ::RDF::Microdata::debug?
-            graph.should be_equivalent_graph(output_graph, self)
-          end
-        end
-      end
-    end
-
-    describe "negative parser tests" do
-      Fixtures::SuiteTest::Bad.each do |m|
-        m.entries.each do |t|
-          specify "#{t.name}: #{t.comment}" do
-            begin
-              t.run_test do
-                lambda do
-                  #t.debug = []
-                   g = RDF::Graph.new
-                   RDF::Microdata::Reader.new(t.input,
-                       :base_uri => t.base_uri,
-                       :validate => true,
-                       :debug => t.debug).each do |statement|
-                     g << statement
-                   end
-                end.should raise_error(RDF::ReaderError)
-              end
+            #puts "parse #{t.query} as #{RDF::Reader.for(t.query)}"
+            output_graph = RDF::Graph.load(t.query, :base_uri => t.data)
+            puts "result: #{CGI.escapeHTML(graph.dump(:ttl))}" if ::RDF::Microdata::debug?
+            if t.result
+              graph.should be_equivalent_graph(output_graph, t)
+            else
+              graph.should_not be_equivalent_graph(output_graph, t)
             end
           end
         end
       end
     end
   end
-
 end
